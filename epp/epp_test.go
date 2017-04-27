539c126e4fb72ae174c8f86ed79994c2105399b6
Jean Mertz <jean@mertz.fm>
2017-04-27 11:01:11 +0200

    switch from Pongo to Sprig library for templating

--- epp/epp_test.go
+++ epp/epp_test.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"os"
 	"testing"
-
-	"github.com/flosch/pongo2"
 )
 
 func init() {
@@ -15,7 +13,7 @@ func init() {
 }
 
 func TestEnvVariables(t *testing.T) {
-	tpl := []byte("{{ SPLIT_TEST }}: {{ KUBERNETES_ADDRESS }}")
+	tpl := []byte(`{{ env "SPLIT_TEST" }}: {{ env "KUBERNETES_ADDRESS" }}`)
 	expected := fmt.Sprintf("%s: %s", os.Getenv("SPLIT_TEST"), os.Getenv("KUBERNETES_ADDRESS"))
 
 	res, err := Parse(tpl)
@@ -31,13 +29,12 @@ func TestEnvVariables(t *testing.T) {
 
 func TestEnvIf(t *testing.T) {
 	tpl := []byte(`
-{% if FALSY %}
+{{- if env "$FALSY" }}
 I shouldn't appear
-{% endif %}
+{{- end }}
 I should!
 `)
 	expected := `
-
 I should!
 `
 
@@ -49,19 +46,5 @@ I should!
 
 	if string(res) != expected {
 		t.Errorf("bad expansion: expected '%s', got '%s'", expected, res)
-	}
-}
-
-func TestFilterBase64Encode(t *testing.T) {
-	actualResult, err := filterBase64Encode(pongo2.AsValue("Hello"), pongo2.AsValue(""))
-
-	if err != nil {
-		t.Errorf("unexpected error '%s'", err)
-	}
-
-	expectedResult := "SGVsbG8="
-
-	if actualResult.String() != expectedResult {
-		t.Errorf("Expected %s but got %s", expectedResult, actualResult)
 	}
 }
